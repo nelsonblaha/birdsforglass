149031a8396dcab748adb15bf7f9189302ce1366
Nelson Blaha <nelson.blaha@gmail.com>
2013-05-08 23:25:48 -0500

    Reduce search distance

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
   function getBirds(position) {
     console.log("starting...");
-    $.get("http://ebird.org/ws1.1/data/obs/geo/recent?lng="+position.coords.longitude+"&lat="+position.coords.latitude+"&fmt=json&dist=10&back=14&includeProvisional=true", function(data) {
+    $.get("http://ebird.org/ws1.1/data/obs/geo/recent?lng="+position.coords.longitude+"&lat="+position.coords.latitude+"&fmt=json&dist=3&back=14&includeProvisional=true", function(data) {
       $.each(data, function(index, bird){
           $.get("image?comName="+encodeURIComponent(bird.comName),function(data2){
             $('div#birds').append("<p><img src='"+data2.url+"'/>"+bird.comName+"</p>");
