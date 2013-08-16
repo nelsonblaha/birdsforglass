4825dc3287a344b1e8367286c07e0d4c786a2c86
Nelson Blaha <nelson.blaha@gmail.com>
2013-08-16 08:13:22 -0500

    Keep session secret out of git

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Birds::Application.config.secret_token = '92093d6c958e7932e63354a4b37eb881cc1508ce340350bc4dc10391169fd872135e1d41fb23390ffd92fcfc297c20e911fdaca4c9aa2e6eb83efd4c888034f0'
+Birds::Application.config.secret_token = ENV["SESSION_SECRET"]
